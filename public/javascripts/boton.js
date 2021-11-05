a42e30ced7147518e1d61a23921de6c089a59a91
Tabitoo <lautarovaldez4@gmail.com>
2021-11-05 16:18:48 -0300

    arreglo de boton

--- public/javascripts/boton.js
+++ public/javascripts/boton.js
@@ -1,6 +1,30 @@
 const bntMenu = document.querySelector('#menu')
 const menuContent = document.querySelector('.menu-content')
 
+let position = 0
+
+
+position = window.screenY
+console.log(position)
+
+
+
+addEventListener("DOMContentLoaded", () => {
+
+    const footer = document.querySelector('.footer')
+
+    let botonStatic = () => {
+        menuContent.classList.toggle('menu-static')
+    }
+
+    const observer = new IntersectionObserver(botonStatic, {
+        threshold: 0.25
+    })
+
+    observer.observe(footer)
+
+})
+
 bntMenu.addEventListener('click', () => {
     menuContent.classList.toggle('menu-active');
 })
